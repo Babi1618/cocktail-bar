4438be28001a52ae128ac0509a9b5b2ba103897f
Babi1618 <b.giuliani1618@gmail.com>
2023-02-22 16:38:58 +0100

    reducer

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,44 +1,54 @@
 import {
-    createContext,
-    PropsWithChildren,
-    useContext,
-    useEffect,
-    useState,
-  } from "react";
-  export const AppContext = createContext({});
-  
-  export const AppContextProvider = (props: PropsWithChildren) => {
-    const [isMobile, setIsMobile] = useState(false);
-    // const [page, setPage] = useState(1);
-  
-    useEffect(() => {
-      const handleResize = () => {
-        if (window.innerWidth < 768) {
-          setIsMobile(true);
-        } else {
-          setIsMobile(false);
-        }
-      };
-      handleResize();
-      window.addEventListener("resize", handleResize);
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }, []);
-  
-    return (
-      <AppContext.Provider
-        value={{
-          isMobile,
-        //   page,
-        //   setPage,
-        }}
-      >
-        {props.children}
-      </AppContext.Provider>
-    );
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
+import reducer from "../utils/reducer";
+export const AppContext = createContext({});
+
+export const AppContextProvider = (props: PropsWithChildren) => {
+  const [isMobile, setIsMobile] = useState(false);
+  const initialState = {
+    cart: [],
   };
-  export const useAppContext = () => {
-    return useContext(AppContext);
+  const [state, dispatch] = useReducer(reducer, initialState) as any;
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const increase = (id: number) => {
+    dispatch({ type: "INCREASE", payload: id });
   };
-  
+  const decrease = (id: number) => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isMobile,
+        ...state,
+        increase,
+        decrease,
+      }}
+    >
+      {props.children}
+    </AppContext.Provider>
+  );
+};
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
